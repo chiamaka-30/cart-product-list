3aae5ee75987b96b42a186252047936e7569ebbc
Chiamaka Onyenma <onyenmachiamakasuccess@gmail.com>
2025-08-10 19:12:42 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import { GeistMono } from 'geist/font/mono'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'My Cart App',
+  title: 'Amaka Cart App',
   description: 'Created with v0',
   generator: 'Custom',
 }
