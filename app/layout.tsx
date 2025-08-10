7e26a922041a7c6fbddeabc60420ce165ba314f5
Chiamaka Onyenma <onyenmachiamakasuccess@gmail.com>
2025-08-10 19:10:54 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,9 @@ import { GeistMono } from 'geist/font/mono'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
+  title: 'My Cart App',
   description: 'Created with v0',
-  generator: 'v0.dev',
+  generator: 'Custom',
 }
 
 export default function RootLayout({
